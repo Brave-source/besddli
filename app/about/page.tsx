1e79df52485b677b4a7eaf45136dd508b663686f
brave-source <eebiyenrin@gmail.com>
2025-03-27 09:15:53 +0100

    add about page

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import Header from '@/components/Header';
+
+export default function About() {
+  return (
+    <main className='bg-[#F9FAFB]'>
+      <Header />
+      <h1>About Us</h1>
+      <p>This is the about page content.</p>
+    </main>
+  );
+}
